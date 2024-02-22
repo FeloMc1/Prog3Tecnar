73e2771d6b7af1d7e4dcc104bfce23056ffacf4a
Felo-Mc <74287866+FeloMc1@users.noreply.github.com>
2024-02-22 11:04:13 -0500

    Delete Estudiantes.py

--- Estudiantes.py
+++
@@ -1,9 +0,0 @@
-class estudiante:
-    def __init__(self, Name="Nombre", Lname="Apellido",Mail="Correo",Phone="telefono"):
-        self.Name = Name
-        self.LName =Lname
-        self.Mail =Mail
-        self.phone=Phone
-        
-    def Getinfo(self):
-        print(self.Name)
